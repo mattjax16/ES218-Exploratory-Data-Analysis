ca109acee953aa0d13c03a87618be0f94e5d77f7
Matt Bass <mnbass21@colby.edu>
2022-02-22 19:18:39 -0500

    hw9 added

--- Homeworks/HW9/HW09.R
+++ Homeworks/HW9/HW09.R
@@ -5,33 +5,37 @@
 # Exploring Colby Registrar Dataset
 
 library(dplyr)
+library(tidyr)
 library(stringr)
 library(lubridate)
 
 # Read data
-dat <- read.csv(paste("https://raw.githubusercontent.com/colby-es218-2022/",
-                      "hw09-mattjax16/1eac26b1a4542894c7f5d30fa63b11506c12",
-                      "a9ea/SP2022.csv?token=GHSAT0AAAAAABRXHR3X6TMW2RRYC7",
-                      "KSZVAGYQVMQRQ",
-                      sep = ""))
+dat <- read.csv("SP2022.csv")
 
 
 # Complete all tasks in a single series of piping operations
-dat1 <- dat %>% 
+# grouping the class]ses by days and get the count of the number of classes
+# in each day
+courses_by_day <- dat %>% 
   mutate(M = str_detect(Days, "M"),
          T = str_detect(Days, "T"),
          W = str_detect(Days, "W"),
          R = str_detect(Days, "R"),
-         F = str_detect(Days, "F"),
-         Overenrolled = case_when(Reg > Max ~ "O",
-                                  Reg == Max ~ "C",
-                                  Reg < Max ~ "U",
-                                  is.na(Reg) | is.na(Max) ~ NA_character_),
-         stime = str_split_fixed(Times, "-",2)[ ,1],
-         etime = str_split_fixed(Times, "-",2)[ ,2],
-         sdate = ymd_hm(paste("2018/1/1", stime)),
-         edate = ymd_hm(paste("2018/1/1", etime)),
-         Duration = (edate - sdate) /dminutes())
+         F = str_detect(Days, "F")) %>% 
+  
+  pivot_longer(cols=c(M,T,W,R,F), names_to = "Day", values_to = "On_Day") %>% 
+  
+  mutate(Day = factor(x = Day, levels = c("M", "T", "W", "R", "F"))) %>% 
+  
+  group_by(Day) %>%
+  summarise(Sum = sum(On_Day))
+
+
+
+saveRDS(courses_by_day, "courses_by_day.rds")
+
+
+
   
 
 
