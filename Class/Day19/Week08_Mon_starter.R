7d87353045dfc908aa25151d857b66d96e91bc90
Matt Bass <mnbass21@colby.edu>
2022-03-28 11:55:08 -0400

    added class

--- Class/Day19/Week08_Mon_starter.R
+++ Class/Day19/Week08_Mon_starter.R
@@ -7,3 +7,30 @@ alto  <- lattice::singer %>%
   arrange(height) %>% 
   mutate(fval = (row_number() - 0.5) / n())
 
+
+# Density plot
+ggplot(alto, aes(height)) + geom_density(fill = "bisque")
+
+
+# Normal Q-Q plot
+ggplot(alto, aes(sample = height)) + 
+  geom_qq(distribution = qnorm) + 
+  geom_qq_line(distribution = qnorm, col  = "red")
+  
+# Simulate normal distribution from alto 1 mean and SD
+x.mean <- mean(alto$height)
+x.sd <- sd(alto$height)
+
+x <- rnorm(n = 35, mean = x.mean, sd = x.sd) # Generate rand height values
+
+# Theoretical Q-Q plot 
+ggplot() + aes(sample = x) + 
+  geom_qq(distribution = qnorm) + 
+  geom_qq_line(distribution = qnorm, col  = "red")
+
+
+
+# Compare with expodential 
+ggplot(alto, aes(sample = height)) + 
+  geom_qq(distribution = qexp) + 
+  geom_qq_line(distribution = qexp, col  = "red")
